310903682702706561f763cf3c193b0c6977e20b
Aman kumar <72993406+AMANJIKUMAR@users.noreply.github.com>
2021-07-05 21:11:57 +0530

    Add files via upload

--- KAVI.py
+++ KAVI.py
@@ -1,28 +1,52 @@
 import pyttsx3
+import speech_recognition as sr
 import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-#print(voices[1].id)
+# print(voices[1].id)
 engine.setProperty('voice', voices[1].id)
+
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour>=0 and  hour<12:
+    if 0 <= hour < 12:
         speak("Good Morning!")
 
-    elif hour>=12 and hour<18:
+    elif 12 <= hour < 18:
         speak("Good Afternoon!")
 
-    else :
+    else:
         speak("Good Evening!")
 
     speak(" I am kavi. Please tell me how may i help you!")
 
-    if __name__== "__main__":
+def takeCommand():
 
-      speak("aman is good boy")    
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing....")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        #print(e)
+
+        print("Say it again please....")
+        return "None"
+
+    return query
+
+    if __name__ == "__main__":
+       wishMe()
+       takeCommand()
